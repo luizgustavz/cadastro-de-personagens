663b4706b956f0aad28f7d4c1729fc018884a89c
luizgustavz <luiz.gus.ms@gmail.com>
2025-09-08 18:36:51 -0300

    inicio do projeto

---
+++ src/main/java/com/luizgustavz/cadastro_de_personagens/cadastroDepersonagens/CadastroDepersonagensApplication.java
@@ -0,0 +1,13 @@
+package com.luizgustavz.cadastro_de_personagens.cadastroDepersonagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDepersonagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDepersonagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizgustavz/cadastro_de_personagens/cadastroDepersonagens/CadastroDepersonagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizgustavz.cadastro_de_personagens.cadastroDepersonagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDepersonagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
