ae8a7b5df4e1fab766c6f2c649bd61205f393da0
luizgustavz <luiz.gus.ms@gmail.com>
2025-09-08 22:15:11 -0300

    feat: cria o perfil de testes

--- src/main/java/com/luizgustavz/cadastro_de_personagens/cadastroDepersonagens/CadastroDepersonagensApplication.java
+++ src/main/java/com/luizgustavz/cadastro_de_personagens/cadastroDepersonagens/CadastroDepersonagensApplication.java
@@ -2,8 +2,10 @@ package com.luizgustavz.cadastro_de_personagens.cadastroDepersonagens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("test")
 public class CadastroDepersonagensApplication {
 
 	public static void main(String[] args) {
