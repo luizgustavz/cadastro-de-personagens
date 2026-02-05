4a24c085a9106c2215bae0c54841442abf54ec68
luizgustavz <luiz.gus.ms@gmail.com>
2026-02-05 19:27:19 -0300

    chore: Adiciona dependencia hibernate validator para validacao de entrada. Adiciona dependencia sprigdoc para documentacao da API

--- src/main/java/com/luizgustavz/cadastrodepersonagens/CadastroDePersonagensApplication.java
+++ src/main/java/com/luizgustavz/cadastrodepersonagens/CadastroDePersonagensApplication.java
@@ -2,10 +2,8 @@ package com.luizgustavz.cadastrodepersonagens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@Profile("test")
 public class CadastroDePersonagensApplication {
 
 	public static void main(String[] args) {
